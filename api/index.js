d181ed7322587efcd7043a48b1234ed02c9ffc90
Mahed Javed <mahedjaved@Maheds-MacBook-Air.local>
2024-06-12 15:49:16 +0100

    finalise connection to mongo

--- api/index.js
+++ api/index.js
@@ -1,7 +1,19 @@
 import express from "express";
 import userRouter from "./routes/user.routes.js";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
+dotenv.config();
 const app = express();
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to Mongo database");
+  })
+  .catch((err) => {
+    console.log("Error : " + err.message);
+  });
+
 // port 3000 is ideal
 app.listen(3000, () => {
   console.log("Listening on port 3000");
