efc5a9a4743945c105d3d6672348ee67e1f88f1c
Mahed Javed <mahedjaved@Maheds-MacBook-Air.local>
2024-06-12 15:34:31 +0100

    adds user schema

---
+++ api/models/user.model.js
@@ -0,0 +1,26 @@
+import mongoose, { Schema } from "mongoose";
+// create a schema
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+// always make it singular
+const User = mongoose.model("User", userSchema);
+
+export default User;
